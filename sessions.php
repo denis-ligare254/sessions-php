cbfd0439c853179bcebcd818fbb285dce54dd091
denis <mrisholigare@gmail.com>
2021-03-19 13:49:12 +0300

    more workings on the backend

--- sessions.php
+++ sessions.php
@@ -1,5 +1,16 @@
 <?php
+// dummy email and password
+   $myemail="mrisholigare@gmail.com";
+   $mypassword="12345";
 if(isset($_POST['login'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $remember = $_POST['remember'];
+if( $mypassword== $password && $myemail==$emai){
+
+}else{
+    echo "wrong credentials,try again!"
+}
 
 }else{
     header("location: sessions.php");
